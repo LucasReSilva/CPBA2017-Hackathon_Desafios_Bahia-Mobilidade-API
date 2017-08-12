6fce00d4da5a942aeffe39cbb15a661d02fb592d
Lucas Silva <lucasre.silva@gmail.com>
2017-08-12 01:31:40 -0300

    neighborhood api generated

---
+++ api/controllers/NeighborhoodController.js
@@ -0,0 +1,11 @@
+/**
+ * NeighborhoodController
+ *
+ * @description :: Server-side logic for managing neighborhoods
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
