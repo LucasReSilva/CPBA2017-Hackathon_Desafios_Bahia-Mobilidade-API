18d512fa0858e69a0ac54e41fd2a185ed91abf33
Lucas Silva <lucasre.silva@gmail.com>
2017-08-12 01:30:43 -0300

    Alert type api genereted

---
+++ api/controllers/AlertTypeController.js
@@ -0,0 +1,11 @@
+/**
+ * AlertTypeController
+ *
+ * @description :: Server-side logic for managing alerttypes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
